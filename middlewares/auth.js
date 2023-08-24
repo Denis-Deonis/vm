3bfd981fa72d0e57504a572590b0d028737d2952
Denis Zykov <denis.zykov.1993@gmail.com>
2023-08-24 12:52:09 +0300

    рабочий проект

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -7,9 +7,9 @@ module.exports.auth = (req, res, next) => {
   const { authorization } = req.headers;
   const token = authorization.replace('Bearer ', '');
 
-  if (!token) {
-    return next(new UnauthorizedError(`Необходима авторизация ${authorization}`));
-  }
+  // if (!authorization || !authorization.startsWith('Bearer ')) {
+  //   return next(new UnauthorizedError('Необходима авторизация'));
+  // }
 
   let payload;
   try {
@@ -18,39 +18,9 @@ module.exports.auth = (req, res, next) => {
       process.env.NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
     );
   } catch (err) {
-    return next(new UnauthorizedError(`Необходима авторизация ${authorization}`));
+    return next(new UnauthorizedError(`Необходима авторизация`));
   }
   req.user = payload;
   return next();
 
 };
-
-
-
-
-// // получаем переменные из .env среды
-// const { NODE_ENV, JWT_SECRET } = process.env;
-
-// // модуль для создания и подтверждения токенов
-// const jwt = require('jsonwebtoken');
-
-// // подключаем 401 ошибку авторизации
-// const { UnathorizedError } = require('../utils/error');
-
-// module.exports.validateToken = (req, res, next) => {
-//   // получаем токен из запроса
-//   const token = req.cookies.jwt;
-//   let payload;
-
-//   try {
-//     // проверяем токен
-//     payload = jwt.verify(
-//       token,
-//       NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
-//     );
-//   } catch (err) {
-//     return next(new UnathorizedError('Authorization required'));
-//   }
-//   req.user = payload;
-//   return next();
-// };
