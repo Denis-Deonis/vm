aa6775b8aba7fdbd8b4f721dc68e8a5eb0ca0f28
Denis Zykov <denis.zykov.1993@gmail.com>
2023-08-24 12:20:24 +0300

    !authorization.startsWith('Bearer ')

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,15 +5,15 @@ const { JWT_SECRET } = process.env;
 
 module.exports.auth = (req, res, next) => {
   const { authorization } = req.headers;
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    return next(new UnauthorizedError(`Необходима авторизация ${authorization}`));
-  }
+  // if (!authorization || !authorization.startsWith('Bearer ')) {
+  //   return next(new UnauthorizedError(`Необходима авторизация ${authorization}`));
+  // }
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = jwt.verify(
       token,
-      process.env.NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-string',
+      process.env.NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
     );
   } catch (err) {
     return next(new UnauthorizedError(`Необходима авторизация ${authorization}`));
